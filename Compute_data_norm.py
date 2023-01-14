ec97fdcc4bbeafef868955633f9210525937910c
alun <come880412@gmail.com>
2023-01-15 00:49:10 +0800

    [200~Compute mean & std of an image dataset~

--- Compute_data_norm.py
+++ Compute_data_norm.py
@@ -1,6 +1,6 @@
 '''
 Getting normalized mean and std for an image dataset:
-python Compute_data_norm.py <image-dir> <image_size>
+python Compute_data_norm.py <image_dir> <image_size>
 '''
 import os
 from glob import glob
